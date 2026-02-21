009b9685d5eeb16e45e3cee05bb1e1426544e495
isak3233 <isak.heimdahl@gmail.com>
2026-02-21 03:15:38 +0100

    Add project files.

---
+++ Models/Region.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoLStatsMaui.Models
+{
+    internal enum Region
+    {
+        EUW,
+        EUNE,
+        NA,
+        KR
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using LoLStatsMaui.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LoLStatsMaui.ViewModels
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+        public ICommand SubmitCommand { get; }
+
+        public Array Regions { get; }
+
+        [ObservableProperty]
+        private string _errorMessage;
+
+        [ObservableProperty]
+        private string _lolName;
+
+        [ObservableProperty]
+        private Models.Region _selectedRegion;
+
+
+        public MainViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+            Regions = Enum.GetValues(typeof(Models.Region));
+        }
+        private void OnSubmit()
+        {
+            if (LolName == null)
+            {
+                ErrorMessage = "Du skrev inget!";
+                return;
+            }
+            if(!LolName.Contains("#"))
+            {
+                ErrorMessage = "Det sökta kontot behöver innehålla #";
+                return;
+            }
+            ErrorMessage = "";
+
+        }
+
+    }
+}
